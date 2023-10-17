422675ff3b1fd7ff219b1abbaa2bfaad63a24f88
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-17 15:26:28 +0300

    Initial Commit

---
+++ Mimicria/MimicriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimicriaApp.swift
+//  Mimicria
+//
+//  Created by Николай Игнатов on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MimicriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
