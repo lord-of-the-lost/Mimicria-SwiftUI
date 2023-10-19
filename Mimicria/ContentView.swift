e5c57b7e7369d3d8583274d197e9070bed2f5b3d
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-19 16:37:30 +0300

    add button

--- Mimicria/ContentView.swift
+++ Mimicria/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isSmallButton = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isSmallButton {
+                Spacer()
+            }
+            RoundedRectangleView(isSmallButton: $isSmallButton)
         }
         .padding()
+    }
+}
+
+struct RoundedRectangleView: View {
+    @Binding var isSmallButton: Bool
+    
+    var body: some View {
+        let width: CGFloat = isSmallButton ? 90 : 300
+        let height: CGFloat = isSmallButton ? 40 : 400
+        
+        HStack {
+            if isSmallButton {
+                Spacer()
+            }
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.blue)
+                .frame(width: width, height: height)
+                .overlay {
+                    ButtonView(isSmallButton: $isSmallButton)
+                }
+        }
+    }
+}
+
+struct ButtonView: View {
+    @Binding var isSmallButton: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    withAnimation(.easeInOut) {
+                        isSmallButton.toggle()
+                    }
+                }) {
+                    ButtonLabel(isSmallButton:  $isSmallButton)
+                        .padding()
+                }
+                if !isSmallButton {
+                    Spacer()
+                }
+            }
+            if !isSmallButton {
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ButtonLabel: View {
+    @Binding var isSmallButton: Bool
+    
+    var body: some View {
+        Label {
+            Text(isSmallButton ? "Open" : "Back")
+        } icon: {
+            if !isSmallButton {
+                Image(systemName: "arrowshape.backward.fill")
+            }
+        }
+        .tint(.white)
     }
 }
 
